df4b416e66fa63875f5e55fd42f92e2b93cba319
will eizlini <w.eizlini@intellisports.net>
2026-02-11 09:35:30 -0600

    Initial Commit

---
+++ TimeTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTracker
+//
+//  Created by Will Eizlini on 2026-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Will Eizlini on 2026-02-11.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
